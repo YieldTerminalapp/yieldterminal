2963547e09cf2d318ac45b4fab3dae1d069a5255
YieldTerminalapp <yieldterminal.io@outlook.com>
2026-04-20 01:47:12 +0200

    resilience: AbortSignal in api client + top-level ErrorBoundary + prod API warn

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -6,15 +6,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SolanaProviders } from './WalletProvider';
+import ErrorBoundary from './components/ErrorBoundary';
 import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <SolanaProviders>
-        <App />
-      </SolanaProviders>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <SolanaProviders>
+          <App />
+        </SolanaProviders>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>
 );
